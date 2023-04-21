5dbbd94100b6264667359d9851b7110942096930
vdkjg <hweichelt@uni-potsdam.de>
2023-04-21 13:23:08 +0200

    rewrote DOPropagatorSingle

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_leveled_list(leveled_list, name=None):
         last_level = l == len(leveled_list) - 1
         print("└──" if last_level else "├──", level[0])
         if len(level) > 1:
-            for i in range(1,len(level)):
+            for i in range(1, len(level)):
                 prefix = " " if last_level else "│"
                 print(f"{prefix}   └──" if i == len(level) - 1 else f"{prefix}   ├──", level[i])
 
@@ -17,16 +17,14 @@ def print_leveled_list(leveled_list, name=None):
 if __name__ == '__main__':
 
     ctl = clingo.Control()
-    dop = DecisionOrderPropagatorSingle("place(2,2)")
+    dop = DecisionOrderPropagatorSingle("place(2,2)", verbose=1)
     ctl.register_propagator(dop)
     ctl.load("logic_programs/pigeon_hole.lp")
     ctl.ground([("base", [])])
     print(ctl.solve(on_model=lambda x: print("+ MODEL FOUND:", x)))
-    print(dop.decision_levels)
-    sym_decision_levels = dop.get_symbolic_decision_levels()
 
-    print()
-    print_leveled_list(sym_decision_levels, name="Solver Decisions")
-
-    print()
-    print("\n".join(sorted([str(s) for s in dop.get_decisions_as_logic_program()])))
+    for decision_levels in dop.get_decision_history():
+        level_symbolic = [[str(dop.get_symbol(lit)) for lit in level] for level in decision_levels]
+        print("\n" + "*"*100 + "\n")
+        print(level_symbolic)
+        print_leveled_list(level_symbolic)
--- propagators.py
+++ propagators.py
@@ -2,7 +2,10 @@ import clingo
 from clingo.symbol import Function
 from clingo.propagator import Propagator
 from clingo.control import Control
+from typing import List
 
+DecisionLevel = List[int]
+DecisionLevelList = List[DecisionLevel]
 
 class DecisionOrderPropagator(Propagator):
 
@@ -85,23 +88,68 @@ class DecisionOrderPropagator(Propagator):
         return output
 
 
-class DecisionOrderPropagatorSingle(DecisionOrderPropagator):
-    def __init__(self, query_atom):
+class DecisionOrderPropagatorSingle:
+
+    def __init__(self, query_atom, verbose=0):
         self.query_atom = clingo.parse_term(query_atom)
+        self.slit_symbol_lookup = {}
+        self.decision_levels_history = []
+        self.verbose = verbose
 
     def init(self, init):
-        self._slit_symbol_lookup = {}
-        print(f"SINGLE D.O. PROPAGATOR: query_atom='{str(self.query_atom)}'")
-        print("SOLVER LITERALS:", sorted([init.solver_literal(a.literal) for a in init.symbolic_atoms]))
         for atom in init.symbolic_atoms:
             program_literal = atom.literal
             solver_literal = init.solver_literal(program_literal)
-            self._slit_symbol_lookup[solver_literal] = atom.symbol
+            self.slit_symbol_lookup[solver_literal] = atom.symbol
 
         if self.query_atom in init.symbolic_atoms:
-            program_literal = init.symbolic_atoms[self.query_atom].literal
-            solver_literal = init.solver_literal(program_literal)
-            init.add_watch(solver_literal)
-            init.add_watch(-solver_literal)
+            query_program_literal = init.symbolic_atoms[self.query_atom].literal
+            query_solver_literal = init.solver_literal(query_program_literal)
+            init.add_watch(query_solver_literal)
+            init.add_watch(-query_solver_literal)
         else:
             raise ValueError("query_atom has to be a valid atom contained in init.symbolic_atoms")
+
+    def propagate(self, control, changes) -> None:
+        decisions, entailments = self.get_decisions(control.assignment)
+        decision_levels_with_entailments = [[d] + list(entailments[d]) if d in entailments else [d] for d in decisions]
+        self.decision_levels_history.append(decision_levels_with_entailments)
+
+    def undo(self, thread_id: int, assignment, changes) -> None:
+        if self.verbose > 0:
+            print("UNDO")
+
+    @staticmethod
+    def get_decisions(assignment):
+        level = 0
+        decisions = []
+        entailments = {}
+        try:
+            while True:
+                decision = assignment.decision(level)
+                decisions.append(decision)
+
+                trail = assignment.trail
+                level_offset_start = trail.begin(level)
+                level_offset_end = trail.end(level)
+                level_offset_diff = level_offset_end - level_offset_start
+                if level_offset_diff > 1:
+                    entailments[decision] = trail[(level_offset_start + 1):level_offset_end]
+                level += 1
+        except RuntimeError:
+            return decisions, entailments
+
+    def get_symbol(self, literal) -> clingo.Symbol:
+        try:
+            if literal > 0:
+                symbol = self.slit_symbol_lookup[literal]
+            else:
+                # negate symbol
+                symbol = clingo.parse_term(f"-{str(self.slit_symbol_lookup[-literal])}")
+        except KeyError:
+            # internal literals
+            symbol = None
+        return symbol
+
+    def get_decision_history(self) -> List[DecisionLevelList]:
+        return self.decision_levels_history
